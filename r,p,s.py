d3f685bf04ee496dc57fb1b5fe3bdc843de8ee0a
tea-ingland <hachio0525@icloud.com>
2024-05-20 20:01:53 +0900

    じゃんけん

---
+++ r,p,s.py
@@ -0,0 +1,15 @@
+print('あなたの手は？ 0:グー,1:チョキ,2:パー')
+A = int(input())
+import random 
+B = random.choice((0,1,2))
+C = {'Aの勝ち'}
+D = {'Bの勝ち'}
+while 
+if A == 0 ^ B == 2:x = D and break
+if B == 0 ^ A == 2:x = C
+else: 
+    if A < B:x = C
+    if A > B:x = D
+    else:continue  
+
+     print: x
